ead9982458c51f69f0c82c92d62af754643230e3
wasanta bhaskara 18-088 <71683907+wasantabhaskara@users.noreply.github.com>
2020-12-19 05:22:54 +0800

    Update jadirelawan.php

--- VOLANCER/jadirelawan.php
+++ VOLANCER/jadirelawan.php
@@ -10,11 +10,28 @@
 
     <title>VOLANCER</title>
     <style>
-        body{
+        html {
+            position: relative;
+        }
+
+        body {
+            margin-bottom: 120px;
+            margin-top: 30px;
             background-color: gainsboro;
         }
-        .jumbotron{
-            height: 100vh;
+
+        .jumbotron img {
+            width:  300px;
+            border: 5px solid #666;
+        }
+
+        hr {
+            width: 250px;
+            border-top: 3px solid #999;
+        }
+
+        section {
+            min-height: 600px;
         }
     </style>
   </head>
@@ -44,65 +61,89 @@
       </div> 
     </div>
     </nav>
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
-                </li>
-                <li>
-                    <a href="#">Dashboard</a>
-                </li>
-                <li>
-                    <a href="#">Shortcuts</a>
-                </li>
-                <li>
-                    <a href="#">Overview</a>
-                </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
+    <div class="jumbotron">
+      <div class="container text-center">
+      <img src="http://rencanamu.id/assets/file_uploaded/editor/1500436949-english-cl.jpg" class="rounded-circle">
+            <h1>Volunteer</h1>
+     </div>
+    </div>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2 class="text-center">Relawan Administrasi</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>Secara klasik, administrasi berarti keperluan yang menyangkut catat-mencatat surat- menyurat. Tapi, kalau di kegiatan relawan, sifatnya lebih ke kegiatan yang lebih formil dan merupakan bagian dari suatu kesatuan unit yang harus dijalankan agar dapat mencapai tujuan. Kegiatan relawan administratif bisa ditemukan di hampir semua organisasi atau lembaga yang menawarkan program relawan, terutama dalam kegiatan yang di balik meja. Contoh program: hampir semua lembaga dan organisasi membuka lowongan dalam program ini. Jatohnya, sih, lebih seperti program magang yang nggak dibayar!</p>
+                </div>
+                
+            </div>
         </div>
-        <!-- /#sidebar-wrapper -->
-
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1>Simple Sidebar</h1>
-                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-                    </div>
+    </section>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2 class="text-center">Relawan Edukasi</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>Dari namanya pasti udah ketebak, deh, ya, kalau kegiatan ini banyak melibatkan kegiatan ajar mengajar. Tapi ternyata nggak cuma sekadar menjadi guru atau tenaga pengajar untuk anak-anak di daerah yang membutuhkan, kegiatan relawan edukatif juga meliputi penyuluhan spesifik megenai topik tertentu untuk memberikan pemahaman terhadap isu yang terjadi dalam lingkungan tersebut serta memberikan solusi aplikatif sekaligus membudayakannya kepada mereka. Contoh program: Greenpeace Indonesia, YAIDS, Indonesia Mengajar</p>
                 </div>
             </div>
         </div>
-        <!-- /#page-content-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <div class="jumbotron">
-      <div class="container">
-        
-     </div>
-    </div>
-    
+    </section>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2 class="text-center">Relawan Konservasi</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>Kepedulian terhadap sekitar nggak harus selalu dalam bentuk bantuan pada sesama manusia aja, kok. Kalau kamu juga turut peduli dengan pelestarian lingkungan seperti binatang dan tumbuhan, kamu bisa banget ikutan program relawan konservasi. Pastinya program konservasi ini menitikberatkan permasalahan titik kepunahan suatu spesies dalam bentuk penangkaran atau eksplorasi lebih jauh untuk meneliti ekosistem tertentu. Contoh program: WWF, Profauna Indonesia</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2 class="text-center">Relawan Humanis</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>Bisa dibilang kegiatan inilah yang membuat kata relawan terdengar familiar di telinga kamu. Kegiatan relawan ini meliputi aksi kemanusiaan yang lebih mendalam seperti penyelamatan pengungsi dan rumah sakit, masyarakat daerah rawan dan terpencil, anak-anak kurang beruntung, korban bencana alam, epidemi penyakit menular, sampai penjaga perdamaian. Kamu yang berminat untuk mengikuti program ini pastinya harus memiliki skill, keyakinan dan komitmen yang ekstra. Contoh program: PMI, United Nation Volunteer</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                <img src="http://rencanamu.id/assets/file_uploaded/editor/1500436975-volunteers.jpg" class="rounded-circle">
+                    <h2 class="text-center">Relawan Acara</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p>Kegiatan relawan satu ini bersifat jauh lebih fleksibel dan seasonal karena hanya diadakan dalam waktu-waktu tertentu oleh suatu pihak, dan tentunya bersifat lebih profesional karena menyangkut program kerja dari suatu lembaga atau organisasi korporat yang lebih besar. Tapi, bukan berarti tanggung jawab yang didapatkan nggak kalah berat, ya! Contoh program: Volunteer konferensi seperti yang diselenggarakan TedX dan Tech in Asia, atau volunteer festival lokal seperti Pesta Rakyat atau Pekan Raya Jakarta.</p>
+                </div>
+            </div>
+        </div>
+    </section>
     <footer class="page-footer font-small bg-warning fixed-bottom">
       <div class="footer-copyright text-center py-3">© 2020 Copyright:
         <a href=#> VOLANCER</a>
